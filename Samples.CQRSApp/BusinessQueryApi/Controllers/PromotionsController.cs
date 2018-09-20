09d0b918066c3a4b44629d139f31e37dc04e0cc1
Jaume Bestard <jaume.bestard@TOOLFACTORY.NET>
2018-09-20 11:42:51 +0200

    QRS sample solution

---
+++ Samples.CQRSApp/BusinessQueryApi/Controllers/PromotionsController.cs
@@ -0,0 +1,27 @@
+﻿using FlexibleSQLProvider;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusinessQueryApi.Controllers
+{
+    [Route("api/business/[controller]")]
+    [ApiController]
+    public class PromotionsController : ControllerBase
+    {
+        private readonly BusinessRepository _businessRepository;
+
+        public PromotionsController()
+        {
+            _businessRepository = new BusinessRepository();
+        }
+
+        [HttpGet("find")]
+        public ActionResult<IEnumerable<string>> GetPromotions(int productId, string productType, DateTime startDate, DateTime endDate)
+        {
+            return _businessRepository.GetDynamicPromotions(productId, productType, startDate, endDate);
+        }
+    }
+}
---
+++ Samples.CQRSApp/FlexibleSQLProvider/BusinessRepository.cs
@@ -0,0 +1,42 @@
+﻿using Dapper;
+using System;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace FlexibleSQLProvider
+{
+    /// <summary>
+    /// Repository containig business query
+    /// </summary>
+    public class BusinessRepository
+    {
+        /// <summary>
+        /// SQL connection to DB
+        /// </summary>
+        private const string SQLConnection = "test.db.anycompany";
+        // samples at http://dapper-tutorial.net/query
+
+            /// <summary>
+            /// Dynamic implementation of a db query
+            /// </summary>
+            /// <param name="productId"></param>
+            /// <param name="productType"></param>
+            /// <param name="startDate"></param>
+            /// <param name="endDate"></param>
+            /// <returns></returns>
+        public dynamic GetDynamicPromotions(int productId, string productType, DateTime startDate, DateTime endDate)
+        {
+            string sql = $"SELECT TOP 10 * FROM Promotions p WITH(NOLOCK) " +
+                $"WHERE p.productId = {productId} " +
+                $"AND p.productType = {productType} " +
+                $"AND p.startDate >= {startDate.ToShortDateString()} " +
+                $"AND p.endDate <= {endDate.ToShortDateString()}";
+
+            using (var connection = new SqlConnection(SQLConnection))
+            {
+                var result = connection.Query(sql).FirstOrDefault();
+                return result;
+            }
+        }
+    }
+}
